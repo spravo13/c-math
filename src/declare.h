d71b7f5f7a15e5bc0e495209fa26a6572fabd7d2
spravo13 <spencer@techandcode.com>
2015-02-09 22:55:12 -0800

    Add header file;includes the function declarations

---
+++ src/declare.h
@@ -0,0 +1,14 @@
+//void degree_to_radian();
+//void radian_to_degree();
+void circle_area();
+//void trapezoid_area();
+//void parallelogram_area();
+//void triangle_area();
+void cylinder_surface_area();
+//void cylinder_volume();
+//void sphere_surface_area();
+//void sphere_volume();
+//void circumference();
+//void arc_length();
+//void sector_area();
+//void quadratic_formula();
