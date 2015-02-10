0bd8da2f17430bf4b32f2b6a30ad841543514033
spravo13 <spencer@techandcode.com>
2015-02-09 22:26:27 -0800

    Add two of the programs for a start.

---
+++ src/circle_area.c
@@ -0,0 +1,18 @@
+#include <math.h>
+#include <stdio.h>
+
+int circle_area() {
+
+	double pi = 3.141592654;
+	float r, a;
+
+	printf("Radius Length: ");
+	scanf("%f",&r);
+
+	a = r*r*pi;
+
+	printf("Area is: %f",a);
+	printf("\n");
+
+	return 0;
+}
---
+++ src/cylinder_surface_area.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+
+void cylinder_surface_area() {
+	
+	double pi = 3.141592654;
+	float r, h, sa;
+
+	printf("Radius of cylinder: ");
+	scanf("%f",&r);
+	printf("Height of cylinder: ");
+	scanf("%f",&h);
+
+	sa = ((2*pi*r) * h) + ((pi*pow(r, 2)*2));
+
+	printf("Surface area is: %f",sa);
+	printf("\n");
+
+}
