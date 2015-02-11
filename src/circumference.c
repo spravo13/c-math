a6fa50f538a7bff38f537b035fa8bdc23824824e
spravo13 <spencer@techandcode.com>
2015-02-10 22:18:05 -0800

    Add circumference program.

---
+++ src/circumference.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdio.h>
+
+void circumference() {
+
+	const double pi = 3.141529654;
+	float d, c;
+
+	printf("Diameter Length: ");
+	scanf("&f",&d);
+
+	c = pi * d;
+
+	printf("circumference: %f",c);
+	printf("\n");
+
+}
