1440096e4fbe3f8a274218575c82adb87e9e9f10
spravo13 <spencer@techandcode.com>
2015-02-10 22:21:59 -0800

    Include circumference in declares file.

--- src/declare.h
+++ src/declare.h
@@ -8,7 +8,7 @@ void cylinder_surface_area();
 //void cylinder_volume();
 //void sphere_surface_area();
 //void sphere_volume();
-//void circumference();
+void circumference();
 //void arc_length();
 //void sector_area();
 //void quadratic_formula();
